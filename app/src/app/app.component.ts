172d949b09da654202026c2e4152017c6ff76c8b
Hamilton Laws <hamiltonlaws233@gmail.com>
2022-08-10 16:48:53 -0400

    messing around with bindings and service

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpService } from './http.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My App';
+
+  userObject ={
+    name:'Jhon',
+    age:'32',
+    id:0
+  }
+
+  newDate = new Date();
+
+  constructor (private HttpService:HttpService) {
+
+  }
+  handleEventHttp(){
+    this.HttpService.getRequest('https://jsonplaceholder.typicode.com/todos/1')
+  }
+
+
+  handleEvent(event:any){
+    console.log(event);
+  }
 }
